7a938bfa60452a1be40479d9af85ebac21cc7508
Felipe Sales <sales.felipeb@gmail.com>
2018-03-20 03:12:27 -0300

    Add Semantic UI

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../../resources/logo.svg';
+import { Container, Header } from 'semantic-ui-react';
+//import logo from '../../resources/logo.svg';
 import './style.css';
 
 class AppContainer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Container fluid>
+            <Header as='h2'>Senhor do tempo</Header>
+      </Container>
+  );
   }
 }
 

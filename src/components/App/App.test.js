19081c1e9236f25445df3ff33f7a492e1a25e694
Felipe Sales <sales.felipeb@gmail.com>
2018-03-25 15:08:44 -0300

    Test component App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
+import sample from '../../resources/api-call.sample.json';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('convert sample api call', () => {
+
+  const appComponent = new App();
+
+  const converted = appComponent.mapForecastInput(sample);
+  const expected = {"coord": {"lat": -16.92, "lon": 145.77}, "stats": {"humidity": "74 %", "tempMax": "27 °C", "tempMin": "27 °C"}, "weather": {"description": "Nuvens esparsas", "icon": "http://openweathermap.org/img/w/03n.png"}};
+
+  expect(converted.stats.humidity).toBe(expected.stats.humidity);
+
+  expect(converted.stats.tempMax).toBe(expected.stats.tempMax);
+
+  expect(converted.weather.icon).toBe(expected.weather.icon);
 });

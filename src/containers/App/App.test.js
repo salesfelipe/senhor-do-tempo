e50c7f1b4e233c84549c3f044e3e93ab13aabeba
Felipe Sales <sales.felipeb@gmail.com>
2018-03-25 16:50:58 -0300

    AppContainer test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,42 +3,49 @@ import ReactDOM from 'react-dom';
 import AppContainer from './index';
 import { shallow } from 'enzyme';
 
+const campinaGrande = {
+  "id": 3403642,
+  "name": "Campina Grande",
+  "country": "BR",
+  "coord": {
+    "lon": -35.881111,
+    "lat": -7.23056
+  }
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AppContainer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-describe('<AppContainer />', () => {
-  it('searchs Campina Grande', () => {
-    const componentWrapper = shallow(<AppContainer />);
+it('searchs Campina Grande', () => {
+  const componentWrapper = shallow(<AppContainer />);
 
-    const campinaGrande = {
-      "id": 3403642,
-      "name": "Campina Grande",
-      "country": "BR",
-      "coord": {
-        "lon": -35.881111,
-        "lat": -7.23056
-      }
-    };
+  expect(componentWrapper.state('searchValue')).toBe('');
+  expect(componentWrapper.state('searchLoading')).toBe(false);
 
-    expect(componentWrapper.state('searchValue')).toBe('');
-    expect(componentWrapper.state('searchLoading')).toBe(false);
+  componentWrapper.instance().handleSearchChange(null, { value: campinaGrande.name });
 
-    // const component = new AppContainer();
+  expect(componentWrapper.state('searchLoading')).toBe(true);
+  expect(componentWrapper.state('searchValue')).toBe(campinaGrande.name);
+})
 
-    // 
 
-    // component.handleSearchChange(null, { value: campinaGrande.name });
+it('resets search', () => {
+  const componentWrapper = shallow(<AppContainer />);
 
-    // expect(component.state.searchValue).toBe(campinaGrande.name);
-    // expect(component.state.searchLoading).toBe(true);
+  expect(componentWrapper.state('searchValue')).toBe('');
+  expect(componentWrapper.state('searchLoading')).toBe(false);
 
-    // setTimeout(function () {
-    //   if (newState == -1) {
-    //     alert('VIDEO HAS STOPPED');
-    //   }
-    // }, 1000);
-  })
-});
+  componentWrapper.setState({ searchLoading: true, select: campinaGrande, searchValue: campinaGrande.name });
+
+  expect(componentWrapper.state('searchLoading')).toBe(true);
+  expect(componentWrapper.state('searchValue')).toBe(campinaGrande.name);
+
+  componentWrapper.instance().resetSearch();
+
+  expect(componentWrapper.state('searchLoading')).toBe(false);
+  expect(componentWrapper.state('searchValue')).toBe('');
+  expect(componentWrapper.state('selected')).toBe(null);
+})
